e3093d0ea928644bc2319f01d6cfff3ec0b7412b
hossain45 <tanjirya79@gmail.com>
2024-09-11 11:15:24 +0600

    Project setup

---
+++ app/index.tsx
@@ -0,0 +1,26 @@
+import { Link, Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { blue } from "react-native-reanimated/lib/typescript/reanimated2/Colors";
+
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text className="text-xl bg-yellow-300 w-full text-center">Hello world!</Text>
+      <StatusBar style="auto" />
+      <Link href="/Profile" style={{ color: "blue", paddingTop: 20 }}>
+        Go To Profile
+      </Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
